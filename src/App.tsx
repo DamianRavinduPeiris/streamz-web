6eac42b1e308af8d09eed86906aac703bbabee4b
Damian Peiris. <drpeiris3@gmail.com>
2023-12-17 14:23:43 +0530

    Installed Daisy UI.

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ function App() {
 
   return (
     <>
-    <div className='flex justify-center align-center'>
+    
       <h1 className="text-7xl font-bold underline">
       Hello world!
     </h1>
-    </div>
+    
     
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,33 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#00b0ff",
+          "primary-focus": "#0091ea",
+          "primary-content": "#ffffff",
+          secondary: "#f50057",
+          "secondary-focus": "#c51162",
+          "secondary-content": "#ffffff",
+          accent: "#ff3d00",
+          "accent-focus": "#dd2c00",
+          "accent-content": "#ffffff",
+          neutral: "#3d4451",
+          "neutral-focus": "#2a2e37",
+          "neutral-content": "#ffffff",
+          "base-100": "#ffffff",
+          "base-200": "#f9fafb",
+          "base-300": "#d1d5db",
+          "base-content": "#1f2937",
+          info: "#2094f3",
+          success: "#009485",
+          warning: "#ff9900",
+          error: "#ff5724",
+        },
+      },
+    ],
+  },
 };
